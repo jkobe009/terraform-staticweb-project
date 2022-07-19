01aeb6f92683140bc1765732cc6b6fe47f0c34db
John Bada <jkobe009@gmail.com>
2022-07-19 00:35:27 -0400

    created route 53 file

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.jupitervpc.id
+}
+
+output "public_subnet_az1_id" {
+  value = aws_subnet.public_subnet_az1.id
+}
+
+output "website_url" {
+  value = aws_subnet.public_subnet_az1.id
+}
--- variable.tf
+++ variable.tf
@@ -86,3 +86,16 @@ variable "ec2_key_pair_name" {
   description = "name of the ec2 key pair"
   type        = string
 }
+
+# route 53 variables
+variable "domain_name" {
+  default     = "aosjupiter.com"
+  description = "domain name"
+  type        = string
+}
+
+variable "record_name" {
+  default     = "www"
+  description = "sub domain name"
+  type        = string
+}
